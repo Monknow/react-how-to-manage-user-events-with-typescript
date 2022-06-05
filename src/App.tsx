985a66bbc4a3d9c595ba40d96596d77f534a3650
Monknow <juanrodriguezcode@gmail.com>
2022-06-04 22:52:22 -0400

    First Commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,29 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import {ButtonHTMLAttributes} from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+interface MessageProps {
+	text: string;
+	onClick: ButtonHTMLAttributes<HTMLButtonElement>["onClick"];
 }
 
-export default App;
+function Message({onClick, text}: MessageProps): JSX.Element {
+	return (
+		<div>
+			<button onClick={onClick}>{text}</button>
+		</div>
+	);
+}
+
+function Game() {
+	return (
+		<div>
+			<Message
+				onClick={() => {
+					alert("I was clicked!");
+				}}
+				text="Click me!"></Message>
+		</div>
+	);
+}
+
+export default Game;
